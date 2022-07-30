9aae7caa40c1dbcc3e13b3f14c8cd59eb6c1950f
Grace Theodore <gracetheo@Graces-MacBook-Pro.local>
2022-07-30 16:54:39 +0800

    add frontend score

---
+++ frontend/src/container/Score.js
@@ -0,0 +1,12 @@
+import './Score.css';
+
+const Score = () => {
+	return (
+		<div class="main-circle">
+			<div class="score-circle" />
+			<div class="white-circle" />
+		</div>
+	)
+}
+
+export default Score
