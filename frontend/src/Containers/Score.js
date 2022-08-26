98c05e3733c5ea1dba0809125d3aa27b6cfe4318
derekdylu <derekdylu@gmail.com>
2022-08-26 23:15:16 +0800

    fixed conflict

--- frontend/src/Containers/Score.js
+++ frontend/src/Containers/Score.js
@@ -1,20 +1,11 @@
 import './Score.css';
-<<<<<<< HEAD
-
-const Score = () => {
-	return (
-		<div class="main-circle">
-			<div class="score-circle" />
-			<div class="white-circle" />
-=======
-import React, { Component }  from 'react';
+import React from 'react';
 
 const Score = () => {
 	return (
 		<div className="main-circle">
 			<div className="score-circle" />
 			<div className="white-circle" />
->>>>>>> grc
 		</div>
 	)
 }
