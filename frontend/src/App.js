9ad6b5ed3e5b1f7c3db6c20fde07634591d251bb
derekdylu <derekdylu@gmail.com>
2022-08-26 23:05:45 +0800

    fixing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,36 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 // Debug
 import Score from './Containers/Score'
 import Navigation from './Components/Navigation'
 import Tutorial from './Components/Tutorial';
-// Containers
 import Game from './Containers/Game'
 import Error from './Containers/Error'
-import React, { Component }  from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-import Score from './Containers/Score';
-import Test from './Containers/Test';
-import Menu from './Containers/Menu';
-import Story from './Containers/Story';
-import Special from './Containers/Special';
+
+// import Test from './Containers/Test';
+// import Menu from './Containers/Menu';
+// import Story from './Containers/Story';
+// import Special from './Containers/Special';
 
 const App = () => {
   return (
       <Router>
         <Routes>
-          {/* Debug */}
           <Route path="/score" element={<Score />} />
           <Route path="/menu" element={<Navigation />} />
           <Route path="/tutorial" element={<Tutorial />} />
 
-          {/* Containers */}
           <Route path="/" element={<Game />} />
           <Route path="*" element={<Error />} />
-          {/* <Route exact path="/feeds" element={<Feeds />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/terms" element={<Terms />} /> */}
-          {/* <Route exact path="/*" element=/{<Home/>} /> */}
+          {/* <Route path="/feeds" element={<Feeds />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/terms" element={<Terms />} /> */}
 
-          {/* <Route exact path="/score" element={<Score />} />
-          <Route exact path="/test" element={<Test />} />
-          <Route exact path="/menu/:type" element={<Menu />} />
-          <Route exact path="/story/:id" element={<Story />} />
-          <Route exact path="/special" element={<Special />} /> */}
+          {/* <Route path="/test" element={<Test />} /> */}
+          {/* <Route path="/menu/:type" element={<Menu />} /> */}
+          {/* <Route path="/story/:id" element={<Story />} /> */}
+          {/* <Route path="/special" element={<Special />} /> */}
         </Routes>
       </Router>
   )
