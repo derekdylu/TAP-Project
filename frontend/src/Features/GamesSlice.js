994bd9b53526e3e4bc3eafa7c69430e536b42b4d
derekdylu <derekdylu@gmail.com>
2022-09-23 14:18:50 +0800

    a

--- frontend/src/Features/GamesSlice.js
+++ frontend/src/Features/GamesSlice.js
@@ -162,8 +162,6 @@ const GamesSlice = createSlice({
 
 export const selectAllGames = (state) => state.games
 
-// TODO select only one game? and update it's cuisine, cart, grocery and score (maybe we don't need score since it will send back to backend?)
-
 export const {gameAdded, 
               gameCuisineUpdated, 
               gameGroceryUpdated,
